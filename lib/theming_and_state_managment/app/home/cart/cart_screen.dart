6a8e1a85799c96e91435903ab50f48a8fd876483
Camilo <juancmaringcu@gmail.com>
2021-10-16 14:55:28 -0500

    Cart p1

--- lib/theming_and_state_managment/app/home/cart/cart_screen.dart
+++ lib/theming_and_state_managment/app/home/cart/cart_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:u_commerce_prototype/theming_and_state_managment/app/theme.dart';
+import 'package:u_commerce_prototype/theming_and_state_managment/app/widgets/delivery_button.dart';
+import 'package:u_commerce_prototype/theming_and_state_managment/data/in_memory_products.dart';
+import 'package:u_commerce_prototype/theming_and_state_managment/domain/model/product.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({Key? key, this.onShoppingg}) : super(key: key);
@@ -7,16 +10,163 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text(
-            'Carrito de Compras',
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              'Carrito de Compras',
+            ),
           ),
         ),
-      ),
-      body: _EmptyCart(
+        body: /* _EmptyCart(
         onShoppingg: onShoppingg,
+      ), */
+            _FullCart());
+  }
+}
+
+class _ShoppingCartProduct extends StatelessWidget {
+  final Product product;
+
+  const _ShoppingCartProduct({Key? key, required this.product})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Stack(
+        children: [
+          Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            color: Theme.of(context).canvasColor,
+            elevation: 8,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                      flex: 2,
+                      child: CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          child: ClipOval(
+                              child: Image.asset(
+                            product.image,
+                            fit: BoxFit.cover,
+                          )))),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                      children: [
+                        Text(product.name),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          product.description,
+                          style: Theme.of(context)
+                              .textTheme
+                              .overline!
+                              .copyWith(color: DeliveryColors.lightGrey),
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              InkWell(
+                                onTap: () {},
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: DeliveryColors.white,
+                                      borderRadius: BorderRadius.circular(4)),
+                                  child: Icon(
+                                    Icons.remove,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 8),
+                                child: Text('0'),
+                              ),
+                              InkWell(
+                                onTap: () {},
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: DeliveryColors.purple,
+                                      borderRadius: BorderRadius.circular(4)),
+                                  child: Icon(
+                                    Icons.add,
+                                    color: DeliveryColors.white,
+                                  ),
+                                ),
+                              ),
+                              const Spacer(),
+                              Text(
+                                '\$${product.price}',
+                                style: TextStyle(color: DeliveryColors.green),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+              right: 0,
+              child: InkWell(
+                child: CircleAvatar(
+                  backgroundColor: DeliveryColors.pink,
+                  child: Icon(Icons.delete_outline),
+                ),
+                onTap: () {},
+              )),
+        ],
       ),
+    );
+  }
+}
+
+class _FullCart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: ListView.builder(
+                  itemCount: products.length,
+                  scrollDirection: Axis.horizontal,
+                  itemExtent: 230,
+                  itemBuilder: (context, index) {
+                    final Product product = products[index];
+                    return _ShoppingCartProduct(
+                      product: product,
+                    );
+                  }),
+            )),
+        Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.yellow,
+            )),
+      ],
     );
   }
 }
